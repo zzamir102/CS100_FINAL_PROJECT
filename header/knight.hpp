84d990808fbdc58eb5807623988b7f2de50d8426
Zamir Noor <zamirnoor98@gmail.com>
2021-05-17 18:45:58 -0700

    Added knight class

---
+++ header/knight.hpp
@@ -0,0 +1,27 @@
+#pragma once
+#ifndef __KNIGHT_HPP__
+#define __KNIGHT_HPP__
+
+#include "character.hpp"
+
+using namespace std;
+
+class Knight : public Character {
+
+public:
+	
+	Knight(const string& name) {
+		this->name = name;
+		type = "Knight";
+		health = 100.0;
+		AttackDamage = 50.0;
+		armor = 10.0;
+		experience = 0;
+		level = 0;
+	}
+
+
+};
+
+
+#endif // !__KNIGHT_HPP__
