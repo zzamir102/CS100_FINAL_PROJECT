9044548e190c399a14ae3232928d854724ce0c68
anoor012 <anoor7122@gmail.com>
2021-05-17 19:21:42 -0700

    Added the Mage and Archer class

---
+++ header/archer.hpp
@@ -0,0 +1,27 @@
+#ifndef __ARCHER__HPP
+#define __ARCHER_HPP__
+
+#include "character.hpp"
+
+using namespace std;
+
+class Archer : public Character {
+
+public:
+
+	Archer(const string& name) {
+		this->name = name;
+		type = "Archer";
+		health = 100;
+		AttackDamage = 60.0;
+		armor = 7.5;
+		experience = 0;
+		level = 0;
+	}
+};
+
+
+
+
+#endif 
+
---
+++ header/mage.hpp
@@ -0,0 +1,23 @@
+#ifndef __MAGE__HPP__
+#define __MAGE__HPP__
+#include "character.hpp"
+
+using namespace std;
+
+class Mage : public Character {
+
+public:
+
+	Mage(const string& _name) {
+		name = _name;
+		type = "Mage";
+		health = 100.0;
+		AttackDamage = 65.0;
+		armor = 5.0;
+		experience - 0;
+		level = 0;
+	}
+
+};
+
+#endif
